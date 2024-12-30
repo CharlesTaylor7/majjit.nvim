9c4e9d935fa93257c564162ab55281e4142425ff
Charles Taylor <charlestaylor95@gmail.com>
2024-12-30 12:58:35 -0500

    hacky

--- lua/majjit/init.lua
+++ lua/majjit/init.lua
@@ -1,5 +1,17 @@
 local M = {}
+
 function M.setup()
   print("majjit.setup(): TODO")
+  vim.uv.fs_mkdir(".jj/majjit/", 777)
+  local fd = vim.uv.fs_open(".jj/majjit/status.jj", "w")
+  vim.print("fd: " .. tostring(fd))
+  local stdout = vim.system({ "jj", "status", "--color", "never" }):wait(3000).stdout
+  vim.uv.fs_write(fd, stdout)
+  ---@cast stdout string
+  local lines = vim.split(stdout, "\n", {})
+  local bufnr = vim.fn.bufadd(".jj/majjit/status.jj")
+  vim.api.nvim_buf_set_lines(bufnr, 0, -1, true, lines)
+  require("chuck.utils").popup(bufnr)
 end
+
 return M
