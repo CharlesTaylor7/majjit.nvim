cb7c0f3358dee10dcbf4a1e88d2e5c88c0970bb4
Charles Taylor <charlestaylor95@gmail.com>
2024-12-30 20:56:18 -0500

    Colourize with baleia

--- lua/majjit/init.lua
+++ lua/majjit/init.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 function M.setup()
+  vim.g.baleia = require("baleia").setup({})
   vim.keymap.set("n", "<leader>jj", M.status, {})
 end
 
@@ -24,14 +25,14 @@ function M.status()
   -- vim.keymap.set("n", "<localleader>ab", M.advance_bookmark, { buffer = buf, desc = "advance bookmark" })
 
   local template = table.concat({ "change_id.short()", "' '", "description", '"\n"', "diff.summary()", '"\n"' }, "++")
-  local cmd = { "jj", "log", "--color", "never", "--no-pager", "--no-graph", "-T", template }
+  local cmd = { "jj", "log", "--no-pager", "--no-graph", "-T", template }
   local log = vim.system(cmd, {}, function(complete)
     local lines = vim.split(complete.stdout, "\n")
     vim.schedule(function()
       vim.print(complete.stderr)
       -- write status
       vim.api.nvim_set_option_value("modifiable", true, { buf = buf })
-      vim.api.nvim_buf_set_lines(buf, 0, -1, true, lines)
+      vim.g.baleia.buf_set_lines(buf, 0, -1, true, lines)
       vim.api.nvim_set_option_value("modifiable", false, { buf = buf })
 
       -- jump to buffer
@@ -43,22 +44,16 @@ end
 function M.show_file_diff()
   local change_id = require("majjit.utils").cursor_word()
   local cursor = vim.api.nvim_win_get_cursor(0)
-  local buf = vim.fn.bufnr()
+  local buf = vim.api.nvim_get_current_buf()
 
-  local diff_tool = 'ui.diff.tool=["difft", "--color=never", "$left", "$right"]'
-  vim.system(
-    { "jj", "show", "--config-toml", diff_tool, "--color", "never", "--no-pager", change_id, "-T", "''" },
-    {},
-    function(complete)
-      local lines = vim.split(complete.stdout, "\n")
-      vim.schedule(function()
-        vim.api.nvim_set_option_value("modifiable", true, { buf = buf })
-        vim.api.nvim_buf_set_lines(buf, cursor[1], cursor[1], true, lines)
-
-        vim.api.nvim_set_option_value("modifiable", false, { buf = buf })
-      end)
-    end
-  )
+  vim.system({ "jj", "show", "--no-pager", change_id, "-T", "''" }, {}, function(complete)
+    local lines = vim.split(complete.stdout, "\n")
+    vim.schedule(function()
+      vim.api.nvim_set_option_value("modifiable", true, { buf = buf })
+      vim.g.baleia.buf_set_lines(buf, cursor[1], cursor[1], true, lines)
+      vim.api.nvim_set_option_value("modifiable", false, { buf = buf })
+    end)
+  end)
 end
 
 --- uses word under cursor as change id
